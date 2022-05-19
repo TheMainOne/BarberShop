f13a9cb019304c28630583f35f76bd2beb6bbe84
Maksym Lvov <83756212+TheMainOne@users.noreply.github.com>
2022-05-19 13:10:16 +0300

    Features markup improved

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -14,36 +14,40 @@ export const Features = () => {
   return (
     <FeaturesSection>
       <Container>
-        <FeaturesText>старая традиционная школа</FeaturesText>
-        <FeaturesTitle>Почему приходят именно к нам?</FeaturesTitle>
-        <FeaturesSecondText>
-          О нас говорят только хорошее. <br />
-          Но лучше 1 раз увидеть и прочувствовать, чем 10 раз прочитать.
-        </FeaturesSecondText>
-        <FeaturesList>
-          <FeaturesListItem>
-            <FeaturesListItemWrapper>600</FeaturesListItemWrapper>
-            <FeaturesListItemText>
-              Довольных клиентов в день
-            </FeaturesListItemText>
-          </FeaturesListItem>
-          <FeaturesListItem>
-            <FeaturesListItemWrapper>50</FeaturesListItemWrapper>
-            <FeaturesListItemText>
-              Наград за отличный сервис
-            </FeaturesListItemText>
-          </FeaturesListItem>
-          <FeaturesListItem>
-            <FeaturesListItemWrapper>20</FeaturesListItemWrapper>
-            <FeaturesListItemText>Лучших мастеров Киева</FeaturesListItemText>
-          </FeaturesListItem>
-          <FeaturesListItem>
-            <FeaturesListItemWrapper>100</FeaturesListItemWrapper>
-            <FeaturesListItemText>
-              Подарков постоянным клиентам
-            </FeaturesListItemText>
-          </FeaturesListItem>
-        </FeaturesList>
+        <div>
+          <div>
+            <FeaturesText>старая традиционная школа</FeaturesText>
+            <FeaturesTitle>Почему приходят именно к нам?</FeaturesTitle>
+            <FeaturesSecondText>
+              О нас говорят только хорошее. <br />
+              Но лучше 1 раз увидеть и прочувствовать, чем 10 раз прочитать.
+            </FeaturesSecondText>
+          </div>
+          <FeaturesList>
+            <FeaturesListItem>
+              <FeaturesListItemWrapper>600</FeaturesListItemWrapper>
+              <FeaturesListItemText>
+                Довольных клиентов в день
+              </FeaturesListItemText>
+            </FeaturesListItem>
+            <FeaturesListItem>
+              <FeaturesListItemWrapper>50</FeaturesListItemWrapper>
+              <FeaturesListItemText>
+                Наград за отличный сервис
+              </FeaturesListItemText>
+            </FeaturesListItem>
+            <FeaturesListItem>
+              <FeaturesListItemWrapper>20</FeaturesListItemWrapper>
+              <FeaturesListItemText>Лучших мастеров Киева</FeaturesListItemText>
+            </FeaturesListItem>
+            <FeaturesListItem>
+              <FeaturesListItemWrapper>100</FeaturesListItemWrapper>
+              <FeaturesListItemText>
+                Подарков постоянным клиентам
+              </FeaturesListItemText>
+            </FeaturesListItem>
+          </FeaturesList>
+        </div>
       </Container>
     </FeaturesSection>
   );
